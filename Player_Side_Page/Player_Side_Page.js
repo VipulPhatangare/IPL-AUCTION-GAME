21d316326d1035e763c6bfa84a8a1de57d99ae8d
VipulPhatangre <vipulphatangare3@gmail.com>
2025-02-22 11:52:48 +0530

    Player side page add new things like bid players and contraint player

--- Player_Side_Page/Player_Side_Page.js
+++ Player_Side_Page/Player_Side_Page.js
@@ -32,3 +32,23 @@ document.getElementById("side_bar_team_name").addEventListener("click",()=>{
     document.getElementById("left_side_bar").style.display = "none";
 });
 
+
+
+document.getElementById("soldBtn").addEventListener("click", function() {
+    document.getElementById("soldStamp").style.display = "block";
+});
+
+document.getElementById("unsoldBtn").addEventListener("click", function() {
+    document.getElementById("unsoldStamp").style.display = "block";
+});
+
+
+
+function navigate_to_team_page(teamId) {
+    if (!teamId) {
+      console.error('Team ID is missing');
+      return;
+    }
+    // Redirect to team.html with the team query parameter
+    window.location.href = "file:///C:/Users/vipul/OneDrive/Desktop/web%20dev/Collage%20projects/IPL%20AUCTION/IPL%20TEAMS/team.html?team_id=" + encodeURIComponent(teamId);
+}
